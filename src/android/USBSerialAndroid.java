907d798b15288d6d302df5a7437d034f766e5de3
ONG KAH KIT <alexlengao@gmail.com>
2019-03-05 17:31:02 +0800

    Changed log: add send master command function

--- src/android/USBSerialAndroid.java
+++ src/android/USBSerialAndroid.java
@@ -2,6 +2,16 @@ package cordova.usbSerialAndroid;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import HelloWorld.Puppy;
+import com.envotech.tablet.ExecuteResultKey;
+import com.envotech.tablet.RFCommandApi;
+
+import android.content.Context;
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,13 +22,46 @@ import org.json.JSONObject;
  */
 public class USBSerialAndroid extends CordovaPlugin {
 
+	private RFCommandApi cmdApi;
+	
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("helloWorld")) {
+			
+			Puppy helloworld = new Puppy();
+			
             String message = args.getString(0) + " - 888";
-            this.coolMethod(message, callbackContext);
+			
+			List<String> cmdList = new ArrayList<>();
+ 
+			cmdList.add("0");
+			
+			
+			cmdApi.getInstance().setContext(this.cordova.getActivity().getApplicationContext());
+ 
+			//Select frequency first, 0 = 2410M
+			cmdApi.getInstance().setCommandFrequency(0);
+			
+			cmdApi.getInstance().masterSendCommonCommand("AC011244", "000000", 530, cmdList);
+			
+            this.coolMethod("hahaha " + Integer.toString(cmdApi.getInstance().getCmdResult()), callbackContext);
+			
             return true;
-        }
+        } else if (action.equals("sendMasterCommand")) {
+			
+			String masterSerialNumber = args.getString(0);
+			String passowrd = args.getString(1);
+			int command = Integer.parseInt(args.getString(2));
+			List<String> cmdList = Arrays.asList(args.getString(3).split(","));
+			
+			cmdApi.getInstance().setContext(this.cordova.getActivity().getApplicationContext());
+			cmdApi.getInstance().setCommandFrequency(0);
+			cmdApi.getInstance().masterSendCommonCommand(masterSerialNumber, passowrd, command, cmdList);
+			
+			this.coolMethod(Integer.toString(cmdApi.getInstance().getCmdResult()), callbackContext);
+			
+            return true;
+		}
         return false;
     }
 
--- www/USBSerialAndroid.js
+++ www/USBSerialAndroid.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.helloWorld = function (arg0, success, error) {
     exec(success, error, 'USBSerialAndroid', 'helloWorld', [arg0]);
 };
+
+exports.sendMasterCommand = function (arg0, arg1, arg2, arg3, success, error) {
+    exec(success, error, 'USBSerialAndroid', 'sendMasterCommand', [arg0, arg1, arg2, arg3]);
+};
