a10318af80a7dc9bdfc0e6fa4fcc70b8bbec2bc1
ONG KAH KIT <alexlengao@gmail.com>
2019-06-18 11:57:50 +0800

    Changed log: remove code for testing

--- src/android/java/android_serialport_api/SerialPort.java
+++ src/android/java/android_serialport_api/SerialPort.java
@@ -42,23 +42,14 @@ public class SerialPort {
 		/* Check access permission */
 		if (!device.canRead() || !device.canWrite()) {
 			try {
-				/* Missing read/write permission, trying to chmod the file */
-				Process su;
-				su = Runtime.getRuntime().exec("/system/bin/su");
-				String cmd = "chmod 666 " + device.getAbsolutePath() + "\n"
-						+ "exit\n";
-				su.getOutputStream().write(cmd.getBytes());
-				if ((su.waitFor() != 0) || !device.canRead()
-						|| !device.canWrite()) {
-					throw new SecurityException();
-				}
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new SecurityException();
 			}
 		}
 
-		mFd = open(device.getAbsolutePath(), baudrate, flags);
+		mFd = open("/dev/ttyMT2", baudrate, flags);
 		if (mFd == null) {
 			Log.e(TAG, "native open returns null");
 			throw new IOException();
